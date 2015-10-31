9894afb35b7868b33a7fd98fb13d59053ce8b084
FsThatOne <fsymbio@icloud.com>
2015-10-31 17:57:23 +0800

    建项目

---
+++ FS3DScrollBanner/FS3DScrollBanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FS3DScrollBanner
+//
+//  Created by FS小一 on 15/10/31.
+//  Copyright © 2015年 FSxiaoyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FS3DScrollBanner/FS3DScrollBannerTests/FS3DScrollBannerTests.swift
@@ -0,0 +1,36 @@
+//
+//  FS3DScrollBannerTests.swift
+//  FS3DScrollBannerTests
+//
+//  Created by FS小一 on 15/10/31.
+//  Copyright © 2015年 FSxiaoyi. All rights reserved.
+//
+
+import XCTest
+@testable import FS3DScrollBanner
+
+class FS3DScrollBannerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
