da9e1eefdec0ea6c0001a129887442ab3809022f
FsThatOne <fsymbio@icloud.com>
2015-11-06 17:30:14 +0800

    完成框架，准备写demo

---
+++ FS3DScrollBanner/FS3DScrollView.swift
@@ -0,0 +1,13 @@
+//
+//  FS3DScrollView.swift
+//  FS3DScrollBanner
+//
+//  Created by FS小一 on 15/11/6.
+//  Copyright © 2015年 FSxiaoyi. All rights reserved.
+//
+
+import Cocoa
+
+class FS3DScrollView: UIView {
+
+}
